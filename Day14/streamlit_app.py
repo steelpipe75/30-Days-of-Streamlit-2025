e08dfe2a9ac4bcaa0807e134f23ddd4bee33a87d
N Y <steelpipe75@gmail.com>
2025-09-21 10:49:06 +0000

    streamlit_ydata_profiling

--- Day14/streamlit_app.py
+++ Day14/streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 # import pandas_profiling
 import ydata_profiling
-from streamlit_pandas_profiling import st_profile_report
+# from streamlit_pandas_profiling import st_profile_report
+from streamlit_ydata_profiling import st_profile_report
 
 st.header('`streamlit_pandas_profiling`')
 
