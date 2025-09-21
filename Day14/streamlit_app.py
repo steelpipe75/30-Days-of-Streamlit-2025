378c5f7280cc9a2bbba5a0550e19cd1d2b1b049f
N Y <steelpipe75@gmail.com>
2025-09-21 09:59:50 +0000

    修正

--- Day14/streamlit_app.py
+++ Day14/streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import pandas_profiling
+# import pandas_profiling
+import ydata_profiling
 from streamlit_pandas_profiling import st_profile_report
 
 st.header('`streamlit_pandas_profiling`')
