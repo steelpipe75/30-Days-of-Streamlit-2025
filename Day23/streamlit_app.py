f7834c920e2687d42afaaed7d72210a34fa174bc
N Y <steelpipe75@gmail.com>
2025-09-29 10:01:26 +0000

    experimental_get_query_params から query_params を修正

--- Day23/streamlit_app.py
+++ Day23/streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-st.title('st.experimental_get_query_params')
+# st.title('st.experimental_get_query_params')
+st.title('st.query_params')
 
 with st.expander('About this app'):
-  st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
+#   st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
+  st.write("`st.query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
 
 # 1. Instructions
 st.header('1. Instructions')
@@ -16,15 +18,22 @@ such that it becomes
 ''')
 
 
-# 2. Contents of st.experimental_get_query_params
-st.header('2. Contents of st.experimental_get_query_params')
-st.write(st.experimental_get_query_params())
+# # 2. Contents of st.experimental_get_query_params
+# 2. Contents of st.query_params
+# st.header('2. Contents of st.experimental_get_query_params')
+st.header('2. Contents of st.query_params')
+# st.write(st.experimental_get_query_params())
+st.write(st.query_params.to_dict())
 
 
 # 3. Retrieving and displaying information from the URL
 st.header('3. Retrieving and displaying information from the URL')
 
-firstname = st.experimental_get_query_params()['firstname'][0]
-surname = st.experimental_get_query_params()['surname'][0]
+query_params = st.query_params.to_dict()
+
+# firstname = st.experimental_get_query_params()['firstname'][0]
+firstname = query_params.get('firstname')
+# surname = st.experimental_get_query_params()['surname'][0]
+surname = query_params.get('surname')
 
 st.write(f'Hello **{firstname} {surname}**, how are you?')
