3cc1612d362e899b1a88ff40abc7b35231e24687
N Y <steelpipe75@gmail.com>
2025-10-07 11:22:53 +0000

    URL変更

--- Day26/streamlit_app.py
+++ Day26/streamlit_app.py
@@ -6,7 +6,8 @@ st.title('🏀 Bored API app')
 st.sidebar.header('Input')
 selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
 
-suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+# suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+suggested_activity_url = f'https://bored-api.appbrewery.com/filter?type={selected_type}'
 json_data = requests.get(suggested_activity_url)
 suggested_activity = json_data.json()
 
